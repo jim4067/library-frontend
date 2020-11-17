705e1335ab4ee97c81e61fc60e9fa8eebfec9129
jim4067 <jimmyimpulse2@gmail.com>
2020-11-17 18:38:40 +0300

    feat: component for showing the Authors

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,38 +1,44 @@
-  
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '../queries';
 
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+	const result = useQuery(ALL_AUTHORS);
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+	if (result.loading) {
+		return <div>loading...</div>
+	}
 
-    </div>
-  )
+	const authors = result.data.allAuthors;
+
+	//this controlls which component is rendered(shown to user). Simple but gets the job done
+	if (!props.show) {
+		return null
+	}
+
+	return (
+		<div>
+
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map((a) => (
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+
+		</div>
+	);
 }
 
-export default Authors
+export default Authors;
