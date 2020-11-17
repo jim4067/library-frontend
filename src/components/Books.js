a590419b4c41ed6b8d10f302aa5254322e01baa8
jim4067 <jimmyimpulse2@gmail.com>
2020-11-17 18:56:46 +0300

    feat: new query for getting all books and using them at the Books componentc

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,38 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+	const result = useQuery(ALL_BOOKS);
 
-  const books = []
+	if (result.loading) {
+		return <div>loading...</div>
+	}
 
-  return (
-    <div>
-      <h2>books</h2>
+	const books = result.data.allBooks;
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+	//this controlls which component is rendered(shown to user). Simple but gets the job done
+	if (!props.show) {
+		return null
+	}
+
+	return (
+		<div>
+
+			<h2>books</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.map((a) => (
+						<tr key={a.title}>
+							<td>{a.title}</td>
+							<td>{a.author}</td>
+							<td>{a.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+
+		</div>
+	);
 }
 
 export default Books
