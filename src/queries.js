1e730c8c7b6ef5d86130f5b2c2c769147787bf29
jim4067 <jimmyimpulse2@gmail.com>
2020-11-17 22:46:09 +0300

    feat: created a query for editing the authors birth year

--- src/queries.js
+++ src/queries.js
@@ -35,3 +35,15 @@ export const CREATE_BOOK = gql`
         }
     }
 `;
+
+export const EDIT_BIRTH = gql`
+    mutation editBirth($name: String!, $setBornTo: String!){
+        editAuthor(
+            name: $name
+            setBornTo: $setBornTo
+        ) {
+            name
+            born
+        }
+    }
+`;
